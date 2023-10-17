f7bed0a55ae3fb108e5a51fa4c01116d4d564a6d
Orisabiyi <orisabiyidavid@gmail.com>
2023-10-17 07:33:44 +0100

    .

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 'use strict';
 // VARIABLES
-const parentMenuBar = document.querySelector('.menu');
-const menuOpen = document.querySelector('.menu__open');
+const menuButton = document.querySelector('.menu');
+const menu = document.querySelector('.menu__open');
 const navMenu = document.querySelector('.nav__menu');
 const overlay = document.querySelector('.overlay');
 
-const menuClose = function () {
-  menuOpen.classList.toggle('menu__open');
-  menuOpen.classList.toggle('menu__close');
+const menuToggle = function (varName) {
+  varName.classList.toggle('menu__open');
+  varName.classList.toggle('menu__close');
 }
 
-parentMenuBar.addEventListener('click', function () {
-  menuClose();
+menuButton.addEventListener('click', function () {
+  menuToggle(menu);
   overlay.classList.toggle('hidden');
 
-  if (menuOpen.classList.contains('menu__close')) {
-    menuOpen.srcset = 'images/icon-close-menu.svg';
+  if (menu.classList.contains('menu__close')) {
+    menu.srcset = 'images/icon-close-menu.svg';
     navMenu.classList.add('nav__menu--new');
   } else {
-    menuOpen.srcset = 'images/icon-menu.svg';
+    menu.srcset = 'images/icon-menu.svg';
     navMenu.classList.remove('nav__menu--new');
   }
-
 })
